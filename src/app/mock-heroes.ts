35127dcbbb1f5f2f6685266f5c75a6ac836343f3
Levi Andrew <levi.andrew98@gmail.com>
2022-11-21 16:23:28 -0300

    heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./heroes/hero.model";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Batman' },
+  { id: 3, name: 'Flash' },
+  { id: 4, name: 'Hulk' },
+  { id: 5, name: 'Deadpool' },
+  { id: 6, name: 'Thor' },
+  { id: 7, name: 'Venom' },
+  { id: 8, name: 'Justiceiro' },
+  { id: 9, name: 'Demolidor' },
+  { id: 10, name: 'Capitão América' },
+]
